tevador/RandomX

Benchmark v1.1.5

src/tests/benchmark.cpp
@@ -158,7 +158,7 @@ int main(int argc, char** argv) {
 
 	store32(&seed, seedValue);
 
-	std::cout << "RandomX benchmark v1.1.4" << std::endl;
+	std::cout << "RandomX benchmark v1.1.5" << std::endl;
 
 	if (help || (!miningMode && !verificationMode)) {
 		printUsage(argv[0]);

ocminer/RandomARQ

src/tests/benchmark.cpp
@@ -158,7 +158,7 @@ int main(int argc, char** argv) {
 
 	store32(&seed, seedValue);
 
-	std::cout << "RandomARQ benchmark v1.1.4" << std::endl;
+	std::cout << "RandomARQ benchmark v1.1.5" << std::endl;
 
 	if (help || (!miningMode && !verificationMode)) {
 		printUsage(argv[0]);